24087ba197e34ab1e75bbfae9e2ccc056b16954c
littlefogcat <littlefogcat@foxmail.com>
2026-07-18 12:14:38 +0800

    fix: K线放大后蜡烛间距过大
    - candlestick添加barMaxWidth:25限制蜡烛最大宽度
    - 添加barMinWidth:2防止K线过多时蜡烛过细
    - 放大时蜡烛宽度达到上限后间距不再等比例增大

--- renderer/js/views/kline-chart.js
+++ renderer/js/views/kline-chart.js
@@ -249,7 +249,7 @@ import * as echarts from 'echarts';
       } else {
         // 蜡烛图模式
         series.push({
-          name: "K线", type: "candlestick", data: ohlc,
+          name: "K线", type: "candlestick", data: ohlc, barMaxWidth: 25, barMinWidth: 2,
           itemStyle: {
             color: "transparent", color0: "#22c55e",
             borderColor: "#ef4444", borderColor0: "#22c55e",
